4b412c83c1a58280e43d9e3e6e835933e4234599
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-09-21 21:11:09 +0300

    Initial commit

---
+++ Vastra/ViewController's/BaseViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BaseViewController.swift
+//  Vastra
+//
+//  Created by Николай Никитин on 21.09.2022.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      view.backgroundColor = .red
+    }
+}
